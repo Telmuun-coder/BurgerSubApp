522597a80626a568fff5fb8dcc2a6cf95af12b79
TelmuuM <telmuunmunkhzorig@gmail.com>
2020-08-19 17:42:07 +0800

    Updates

---
+++ precache-manifest.64e00307d36f8c541e424c2fd529e63f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16da7e6284b5d92d0aade046eb6c0f3",
+    "url": "/BurgerSubApp/index.html"
+  },
+  {
+    "revision": "fe6756a235c0ca2b341e",
+    "url": "/BurgerSubApp/static/css/main.616b6ba4.chunk.css"
+  },
+  {
+    "revision": "ac8e438fbba0626c4e26",
+    "url": "/BurgerSubApp/static/js/2.e82b40cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BurgerSubApp/static/js/2.e82b40cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6756a235c0ca2b341e",
+    "url": "/BurgerSubApp/static/js/main.38eb491e.chunk.js"
+  },
+  {
+    "revision": "a6605d61247cbe3c9956",
+    "url": "/BurgerSubApp/static/js/runtime-main.f2bc79c9.js"
+  }
+]);
